188b2c4550f4fe66eb6788f56541687e6a2983c0
llaruina <simpluis2@hotmail.com>
2024-08-31 21:34:25 -0300

    Hecho el circuito basico

--- carrito.js
+++ carrito.js
@@ -2,6 +2,8 @@ const main = document.getElementById("main");
 
 const resumen = document.getElementById("resumen");
 
+const datosEnvio = document.getElementById("datosEnvio");
+
 let total = 0
 
 
@@ -29,7 +31,48 @@ carritoGuardado.forEach(el => {
 })
 
 
-resumen.innerHTML = `<h2>El total a pagar es: $${total}</h2>`;
+
+resumen.innerHTML = `<h2>El total a pagar es: $${total}</h2>
+<button class="btnComprar"> Finalizar compra </button>
+`;
+
+
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.matches('.btnComprar')) {
+        const envio = `
+            <div class="contenedor">
+                <p> 
+                    <label> Nombre:
+                    <input type="text" id="nombre"> 
+                </p> 
+
+                <p> 
+                    <label> Direccion:
+                    <input type="text" id="direccion"> 
+                </p> 
+
+                <p> 
+                <label> Email:
+                <input type="text" id="email"> 
+                </p> 
+
+                <button class="btnEnviar"> Enviar </button>
+            </div>
+            `;
+        datosEnvio.innerHTML = envio;
+        console.log('Formulario de envío añadido');
+    }
+
+    if (e.target && e.target.matches('.btnEnviar')) {
+        console.log("Boton enviar clic")
+        const volver = `
+            <div class="contenedor">
+                <a href="index.html"> Volver al Inicio</a>  
+            </div>
+            `;
+        datosEnvio.innerHTML += volver;
+    }
+});
 
 function calcularTotal(producto, precio, cantidad = 1) {
     total += precio * cantidad
